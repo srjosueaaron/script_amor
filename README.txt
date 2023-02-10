789d069e923517a279692a7e8b8541a604634a4b
Josue Aaron Rivero <sr.josue.aron@gmail.com>
2023-02-10 09:16:50 -0600

    SCRIPT_AMOR

---
+++ README.txt
@@ -0,0 +1,31 @@
+¿CÓMO UTILIZAR EL PROYECTO?
+Opción Uno:  Simplemente ingresa aquí -> https://script-amor.srjosueaaron.com/
+Opción Dos:  Descargar el archivo APK e instarlo en tu dispositivo Android
+Opción Tres: Descargar el archivo ZIP desde GitHub
+
+Aprovechando la ocasión cercana al 14 de Febrero les presento una 
+versión modificada del proyecto donde le puedes pedir a alguien
+donde se muestran dos botones uno de SÍ y otro de NO.
+Al dar clic o colocarse sobre el botón de "NO" se moverá aleatoriamente
+en la pantalla.
+
+Funciones principales
+-Al inicio pida el nombre de la persona y se imprime en pantalla
+-El diseño de la interfaz fue basado en el meme del cartel con sonido de banda de fondo
+-El proyecto es responsivo para que se pueda visualizar tanto en 
+dispositivos moviles o en una computadora
+
+Se utilizaron simplemente tres archivos uno de HTML, otro de CSS y el 
+último de JavaScript para dar la funcionalidad.
+
+Les recuerdo que el código mostrado no fue totalmente creado por mí 
+solamente modificado para quedarse lo mejor de tres videos que 
+encontre en YouTube sobre el tema. Eres libre de utilizar el código
+para lo que necesites o modificar a tus necesidades especificas.
+
+VIDEOS EN LOS QUE ME BASE PARA CREAR EL PROYECTO
+1.
+2.
+
+ME PUEDES ENCONTRARME EN TODAS LAS REDES SOCIALES COMO: @srjosueaaron
+
---
+++ index.js
@@ -0,0 +1,39 @@
+var bienvenida = "Escribe tu hermoso nombre 💖✨: ";
+var data = prompt(bienvenida);
+
+//INTENTE COLOCAR UNA CANCION QUE SE REPRODUZCA
+var cancion = new Audio("musica.mp3");
+cancion.loop = true;
+cancion.play();
+
+//Imprimir el nombre de la persona y transformalo a mayúsculas
+document.getElementById("data").innerHTML = data.toUpperCase();
+
+const yesBtn = document.querySelector('#yesBtn');
+
+//ESCRIBIR LO QUE PASARA EN CASO DE QUE SÍ
+yesBtn.addEventListener('click', function(){
+    alert('Siempre supe que aceptarias'); //Puedes mandar los mensajes que quieras
+    location.href = 'https://www.youtube.com/watch?v=0uZdFmbGGaU'; //Cambiar el URL en YouTube de la canción que quieras 
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+//NO MOVER NADA AQUÍ
+noBtn.addEventListener('mouseover', function(){
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+
+    noBtn.style.setProperty('top', randomY+'%');
+    noBtn.style.setProperty('left', randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+});
+
+noBtn.addEventListener('click', function(){
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+
+    noBtn.style.setProperty('top', randomY+'%');
+    noBtn.style.setProperty('left', randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+});
